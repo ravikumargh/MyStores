6f34aabd656e9149eeaf8d063e84839fd8e6e1bc
Ravikumar GH <ravikumargh13@gmail.com>
2016-03-03 20:16:29 +0530

    Store , add modal popup

--- public/modules/stores/controllers/stores.client.controller.js
+++ public/modules/stores/controllers/stores.client.controller.js
@@ -1,21 +1,29 @@
 'use strict';
 
-angular.module('stores').controller('StoresController', ['$scope', '$stateParams', '$location', 'Authentication', 'Stores', 'File',
-	function($scope, $stateParams, $location, Authentication, Stores, File) {
+angular.module('stores').controller('StoresController', 
+		['$scope', '$stateParams', '$location', '$modal', '$log', '$timeout', 'Authentication', 'Stores', 'File',
+	function($scope, $stateParams, $location, $modal, $log, $timeout, Authentication, Stores, File) {
 		$scope.authentication = Authentication;
 
-		$scope.create = function() {
-			var store = new Stores({
-				name: this.name,
-				address: this.address
-			});
+		$scope.create = function(store, file) {
+			// var store = new Stores({
+			// 	name: this.name,
+			// 	address: this.address
+			// });
 			store.$save(function(response) {
-				$location.path('stores/' + response._id);
+				$timeout(function () {                    
+					$scope.stores.unshift(response);
+                },1000);
+				// $location.path('stores/' + response._id);
 
-				$scope.name = '';
-				$scope.address = '';
+				// $scope.name = '';
+				// $scope.address = '';
 			}, function(errorResponse) {
 				$scope.error = errorResponse.data.message;
+			}).then(function(res){
+				File.addNewFile(file, res._id).then(function(res){
+				 
+          		});
 			});
 		};
 
@@ -66,7 +74,59 @@ angular.module('stores').controller('StoresController', ['$scope', '$stateParams
             }
          };
 
+        $scope.openCreateModal = function (item) {
+			$scope.selectedOffer=item;
+		    var modalInstance = $modal.open({
+		      animation: $scope.animationsEnabled,
+		      templateUrl: 'modules/stores/views/create-store.client.view.html',
+		      controller: 'StoresModalController',
+		       
+		      resolve: {
+		        ParentScope: function () {
+		          return $scope;
+		        }
+		      }
+		    });
+
+		    modalInstance.result.then(function (selectedItem) {
+		      $scope.selected = selectedItem;
+		    }, function () {
+		      $log.info('Modal dismissed at: ' + new Date());
+		    });
+	    }; 
          
-         
+	}
+]);
+
+
+angular.module('stores').controller('StoresModalController',
+['$scope', '$stateParams', '$location', 'Authentication', 'Stores', 'File', '$modalInstance', 'ParentScope',
+	function($scope, $stateParams, $location, Authentication, Stores, File, $modalInstance, ParentScope) {
+ 
+		$scope.authentication = Authentication;
+		
+		$scope.create = function() {
+			var store = new Stores({
+				name: this.name,
+				address: this.address
+			});
+			ParentScope.create(store, $scope.file);
+			$modalInstance.dismiss('cancel');
+		};
+
+  
+		$scope.update = function() {
+			var offer = $scope.offer;
+
+			offer.$update(function() {
+				$location.path('offers/' + offer._id);
+			}, function(errorResponse) {
+				$scope.error = errorResponse.data.message;
+			});
+		};
+
+		$scope.cancel = function () {
+		    $modalInstance.dismiss('cancel');
+		};
 	}
 ]);
